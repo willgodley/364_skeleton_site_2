ea3e03205424ba0dc36bb123bd2200597ef73a5c
Will Godley <wmgodley@umich.edu>
2019-02-07 05:13:59 +0000

    updated while stuck on author

--- catalog/urls.py
+++ catalog/urls.py
@@ -5,7 +5,7 @@ urlpatterns = [
     path('books/', views.BookListView.as_view(), name='books'),
     path('book/<int:pk>', views.BookDetailView.as_view(), name='book-detail'),
     path('authors/', views.AuthorListView.as_view(), name='authors'),
-    path('author/,int:pk>', views.AuthorDetailView.as_view(), name='author-detail')
+    path('author/<int:pk>', views.AuthorDetailView.as_view(), name='author-detail')
 ]
 
 # New lines below to serve static files in debug mode
